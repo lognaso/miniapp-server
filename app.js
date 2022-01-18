fcb39753cde72e234460c93cf753579877ecf72a
Le Van Long <longlv.th@gmail.com>
2022-01-18 11:33:23 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var app = express()
 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
 app.get('/', function (req, res) {
     res.send('hello world')
 })
